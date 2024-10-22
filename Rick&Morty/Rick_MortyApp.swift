9acff82d667dca12d9db8aa15e758a6f3ab11d0e
irinadeeva <deeva97@me.com>
2024-10-22 13:40:59 +0300

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Irina Deeva on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
